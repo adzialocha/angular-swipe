428f5226948e390c9f6a34664d39fee7fee4927f
Andreas Dzialocha <andreas@marmorkuchen.net>
2013-11-13 23:41:23 +0100

    initial commit 0.0.1

---
+++ dist/angular-swipe.js
@@ -0,0 +1,148 @@
+(function(window, angular, undefined) {
+
+  'use strict';
+
+  /* global -ngSwipe */
+
+  var ngSwipe = angular.module('ngSwipe', []);
+
+  ngSwipe.factory('$swipe', [ function() {
+
+    // The total distance in any direction before we make the call on swipe
+
+    var MOVE_BUFFER_RADIUS = 10;
+
+    function getCoordinates(event) {
+      var touches = event.touches && event.touches.length ? event.touches : [event];
+      var e = (event.changedTouches && event.changedTouches[0]) ||
+          (event.originalEvent && event.originalEvent.changedTouches &&
+              event.originalEvent.changedTouches[0]) ||
+          touches[0].originalEvent || touches[0];
+
+      return {
+        x: e.clientX,
+        y: e.clientY
+      };
+    }
+
+    return {
+
+      bind: function(element, eventHandlers) {
+
+        // Absolute total movement
+        var totalX, totalY;
+        // Coordinates of the start position.
+        var startCoords;
+        // Last event's position.
+        var lastPos;
+        // Whether a swipe is active.
+        var active = false;
+
+        element.on('touchstart mousedown', function(event) {
+          startCoords = getCoordinates(event);
+          active = true;
+          totalX = 0;
+          totalY = 0;
+          lastPos = startCoords;
+          eventHandlers['start'] && eventHandlers['start'](startCoords, event);
+        });
+
+        element.on('touchcancel', function(event) {
+          active = false;
+          eventHandlers['cancel'] && eventHandlers['cancel'](event);
+        });
+
+        element.on('touchmove mousemove', function(event) {
+
+          if (!active) return;
+          if (!startCoords) return;
+
+          var coords = getCoordinates(event);
+
+          totalX += Math.abs(coords.x - lastPos.x);
+          totalY += Math.abs(coords.y - lastPos.y);
+
+          lastPos = coords;
+
+          if (totalX < MOVE_BUFFER_RADIUS && totalY < MOVE_BUFFER_RADIUS) {
+            return;
+          }
+
+          event.preventDefault();
+          eventHandlers['move'] && eventHandlers['move'](coords, event);
+
+        });
+
+        element.on('touchend mouseup', function(event) {
+          if (!active) return;
+          active = false;
+          eventHandlers['end'] && eventHandlers['end'](getCoordinates(event), event);
+        });
+      }
+    };
+  }]);
+
+  function makeSwipeDirective(directiveName, direction, axis, eventName) {
+    ngSwipe.directive(directiveName, ['$parse', '$swipe', function($parse, $swipe) {
+
+      var MAX_OTHER_AXIS_DISTANCE = 75;
+      var MAX_RATIO = 0.3;
+      var MIN_DISTANCE = 30;
+
+      return function(scope, element, attr) {
+
+        var swipeHandler = $parse(attr[directiveName]);
+
+        var startCoords, valid;
+
+        function validSwipe(coords) {
+
+          if (!startCoords || !valid) return false;
+
+          var deltaY = (coords.y - startCoords.y) * direction;
+          var deltaX = (coords.x - startCoords.x) * direction;
+
+          if (! axis){  // horizontal swipe
+            return Math.abs(deltaY) < MAX_OTHER_AXIS_DISTANCE &&
+              deltaX > 0 &&
+              deltaX > MIN_DISTANCE &&
+              Math.abs(deltaY) / deltaX < MAX_RATIO;
+          } else {  // vertical swipe
+            return Math.abs(deltaX) < MAX_OTHER_AXIS_DISTANCE &&
+              deltaY > 0 &&
+              deltaY > MIN_DISTANCE &&
+              Math.abs(deltaX) / deltaY < MAX_RATIO;
+          }
+
+        }
+
+        $swipe.bind(element, {
+          'start': function(coords, event) {
+            startCoords = coords;
+            valid = true;
+          },
+          'cancel': function(event) {
+            valid = false;
+          },
+          'end': function(coords, event) {
+            if (validSwipe(coords)) {
+              scope.$apply(function() {
+                element.triggerHandler(eventName);
+                swipeHandler(scope, { $event: event });
+              });
+            }
+          }
+        });
+      };
+    }]);
+  }
+
+  // Left is negative X-coordinate, right is positive
+
+  makeSwipeDirective('ngSwipeLeft', -1, false, 'swipeleft');
+  makeSwipeDirective('ngSwipeRight', 1, false, 'swiperight');
+
+  makeSwipeDirective('ngSwipeUp', -1, true, 'swipeup');
+  makeSwipeDirective('ngSwipeDown', 1, true, 'swipedown');
+
+})(window, window.angular);
